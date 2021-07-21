f5835ade39624afabcf67cad157e607e122ff763
nandono206 <79740572+nandono206@users.noreply.github.com>
2021-07-21 20:02:14 +0700

    add carousel

--- src/components/LandingPageComponents/Carousel.jsx
+++ src/components/LandingPageComponents/Carousel.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
+import "./assets/Carousel.css"
+import { useState } from "react";
+import Slider from "react-slick";
+import NightPre from "./assets/NightPre";
+import BusChal from "./assets/BusChal";
+import BusClass from "./assets/BusClass";
 
-const Carousel = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const elements = [<NightPre />, <BusChal />, <BusClass />, <NightPre />, <BusChal />];
+
+function Carousel() {
+
+  const [Index, setIndex] = useState(0);
+
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: 0,
+    slidesToScroll: 1,
+    beforeChange: (current, next) => setIndex(next),
+    responsive: [
+      {
+        breakpoint: 930,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          centerPadding: "60px",
+          dots: true,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="Car">
+      <Slider {...settings}>
+        {elements.map((element, idx) => (
+          <div className={idx === Index ? "slide activeSlide" : "slide"}>
+            {element}
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
+
+ 
 
 export default Carousel
