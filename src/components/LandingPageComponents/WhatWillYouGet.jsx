c86c79b0ea84fc74693216bc44c70bf15ef15f9c
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2021-07-28 16:21:46 +0800

    add framer motion

--- src/components/LandingPageComponents/WhatWillYouGet.jsx
+++ src/components/LandingPageComponents/WhatWillYouGet.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Text from "../shared/Text";
 import { useMediaQuery } from "react-responsive";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const BenefitPage = styled.div`
   display: flex;
@@ -32,35 +34,61 @@ const Image = styled.img`
   height: 200px;
 `;
 
+const dataCards = [
+  {
+    name: "Business Knowledge",
+    imageUrl: "https://campbelltaylorwashburn.com/wp-content/uploads/2019/07/Consulting-Illustration.png",
+  },
+  {
+    name: "Hands On Business",
+    imageUrl: "https://campbelltaylorwashburn.com/wp-content/uploads/2019/07/Consulting-Illustration.png",
+  },
+  {
+    name: "Networking",
+    imageUrl: "https://campbelltaylorwashburn.com/wp-content/uploads/2019/07/Consulting-Illustration.png",
+  },
+];
+
 const WhatWillYouGet = () => {
   const isCol = useMediaQuery({
     query: "(min-width: 918px)",
   });
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  const title = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+  const card = {
+    hidden: { opacity: 0, y: -90 },
+    visible: { opacity: 1, y: 0 },
+  };
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
     <BenefitPage>
-      <Text type="primary" size={3} style={{ marginBottom: "5rem", textAlign: "center" }}>
-        What Will You Get
-      </Text>
-      <Card style={isCol ? {} : { flexDirection: "column" }}>
-        <BenefitCard>
-          <Image src="https://campbelltaylorwashburn.com/wp-content/uploads/2019/07/Consulting-Illustration.png" />
-          <Text type="secondary" style={{ fontSize: "28px" }}>
-            Business Knowledge
-          </Text>
-        </BenefitCard>
-        <BenefitCard>
-          <Image src="https://campbelltaylorwashburn.com/wp-content/uploads/2019/07/Consulting-Illustration.png" />
-          <Text type="secondary" style={{ fontSize: "28px" }}>
-            Hands On Business
-          </Text>
-        </BenefitCard>
-        <BenefitCard>
-          <Image src="https://campbelltaylorwashburn.com/wp-content/uploads/2019/07/Consulting-Illustration.png" />
-          <Text type="secondary" style={{ fontSize: "28px" }}>
-            Networking
-          </Text>
-        </BenefitCard>
-      </Card>
+      <motion.h1 ref={ref} animate={controls} initial="hidden" variants={title} transition={{ type: "spring", stiffness: 20 }}>
+        <Text type="primary" size={3} style={{ marginBottom: "5rem", textAlign: "center" }}>
+          What Will You Get
+        </Text>
+      </motion.h1>
+      <motion.div ref={ref} animate={controls} initial="hidden" variants={card} transition={{ type: "spring", stiffness: 100 }}>
+        <Card style={isCol ? {} : { flexDirection: "column" }}>
+          {dataCards.map((data) => (
+            <motion.div whileHover={{ y: -50 }} transition={{ type: "spring", stiffness: 70 }}>
+              <BenefitCard style={{ cursor: "pointer" }}>
+                <Image src={data.imageUrl} />
+                <Text type="secondary" style={{ fontSize: "28px" }}>
+                  {data.name}
+                </Text>
+              </BenefitCard>
+            </motion.div>
+          ))}
+        </Card>
+      </motion.div>
     </BenefitPage>
   );
 };
