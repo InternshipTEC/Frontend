def05daf1f5febcd38fada4ca299607ea6b41aca
adityarip <adityaariputra869@gmail.com>
2021-07-28 10:05:00 +0800

    Update Jumbotron.jsx

--- src/components/LandingPageComponents/Jumbotron.jsx
+++ src/components/LandingPageComponents/Jumbotron.jsx
@@ -4,6 +4,8 @@ import Text from '../shared/Text'
 import { Link } from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 import { useMediaQuery } from 'react-responsive'
+import { motion,useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 
 const Jumtron = styled.div`
@@ -80,9 +82,29 @@ const Jumbotron = () => {
           </>
         );
       });
+    
+    const parg = {
+        hidden: {opacity: 0,y: 30},
+        visible: {opacity: 1, y: 0}
+    }
+
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(()=>{
+        if(inView){
+            controls.start('visible')
+        }
+    }, [controls,inView]);
+    
     return (
         <>
             <Jumtron>
+                <motion.div
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1, y: -80 }}
+                transition={{ duration : 2 }}
+                >
                 <Text size={2.75} type="primary">
                     WELCOME TO TEC INTERNSHIP
                 </Text>
@@ -97,12 +119,18 @@ const Jumbotron = () => {
                         </NavLink>
                     </LinkWrapper>
                 </Text>
-                <div/>
                 <RegistrationDeadline>
                     <Text size={1.25} type="secondary" color="white">Registration will be closed in &nbsp;</Text>
                     <Text size={1.25} type="secondary"> {timerComponents} </Text>
                 </RegistrationDeadline>
+                </motion.div>
             </Jumtron>
+            <motion.p
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            transition={{duration:0.5}}
+            variants={parg}>
             <Parg mobile={isMobile}>
                 <Text type="primary" size={3}>
                     TEC INTERNSHIP
@@ -111,6 +139,7 @@ const Jumbotron = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </Text>
             </Parg>
+            </motion.p>
         </>
     )
 }
